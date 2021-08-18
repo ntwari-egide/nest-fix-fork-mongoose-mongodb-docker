b5463fe1432d802fb14ab35ddd24d1a4db4212c8
ntwari <ntwariegide2@gmail.com>
2021-08-18 13:32:46 +0200

    fixed bug in post comment model

--- src/providers/post-comment.provider.ts
+++ src/providers/post-comment.provider.ts
@@ -1,10 +1,11 @@
 import { Connection } from "mongoose";
+import { PostCommentSchema } from "src/models/post-comment.model ";
 import { UserSchema } from "src/models/user.model";
 
 export const postCommentProviders = [
     {
         provide: 'POST-COMMENT',
-        useFactory: (connection: Connection) => connection.model('User', UserSchema),
+        useFactory: (connection: Connection) => connection.model('Post-comment', PostCommentSchema),
         inject: ['DATABASE_CONNECTION'],
     }
 ]
