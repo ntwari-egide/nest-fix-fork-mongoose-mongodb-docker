b150462b330fc353217195a4d2c2cf4fa4b9f6fa
ntwari <ntwariegide2@gmail.com>
2021-08-18 12:43:40 +0200

    added new api

--- src/controllers/post-type.controller.ts
+++ src/controllers/post-type.controller.ts
@@ -1,4 +1,5 @@
 import { Post } from '@nestjs/common';
+import { Get } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
 import CreatePostTypeDTO from 'src/dtos/create-post-type.dto';
 import { PostTypeService } from 'src/services/post-type.service';
@@ -11,6 +12,11 @@ export class PostTypeController {
     @Post()
     async createNewPostType(newPostType: CreatePostTypeDTO){
         return this.postTypeService.addNewPostType(newPostType);
+    }
+
+    @Get()
+    async getAll(){
+        return this.postTypeService.findAll()
     }
 
 }
