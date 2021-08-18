e620ef80647be6ba6a7ecb2785fb3d5e04c99b6c
ntwari <ntwariegide2@gmail.com>
2021-08-18 13:10:13 +0200

    added post not found exception handler

---
+++ src/exceptions/PostNotFoundException.ts
@@ -0,0 +1,10 @@
+import { HttpException, HttpStatus } from "@nestjs/common";
+
+export class PostNotFoundException extends HttpException{
+    constructor(message: String){
+        super({
+            status: HttpStatus.NOT_FOUND,
+            error: message
+        },HttpStatus.NOT_FOUND)
+    }
+}
