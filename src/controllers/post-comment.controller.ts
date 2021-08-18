076d314b2b8107a0d085c51a51777941cd2e17c6
ntwari <ntwariegide2@gmail.com>
2021-08-18 14:04:10 +0200

    added new apis in post comment controller

--- src/controllers/post-comment.controller.ts
+++ src/controllers/post-comment.controller.ts
@@ -1,4 +1,50 @@
+import { Body, Get, Post, Put } from '@nestjs/common';
+import { Param } from '@nestjs/common';
+import { Delete } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
+import { CreatePostCommentDTO } from 'src/dtos/create-post-comment.dto';
+import { PostCommentModel } from 'src/interface/post-comment.interface';
+import { PostCommentService } from 'src/services/post-comment.service';
 
-@Controller('post-comment')
-export class PostCommentController {}
+@Controller('/api/v1/post-comments')
+export class PostCommentController {
+
+    constructor(private readonly postCommentService: PostCommentService) {}
+
+    @Get()
+
+    async getAll(): Promise<PostCommentModel[]>{
+
+        return await this.postCommentService.getAll()
+
+    }
+
+    @Get("/:id")
+    async get(@Param("id") id: String): Promise<PostCommentModel>{
+
+        return await this.postCommentService.getById(id)
+    }
+
+
+    @Post()
+    async save(@Body() newComment: CreatePostCommentDTO): Promise<PostCommentModel>{
+
+        return await this.postCommentService.postComment(newComment)
+    }
+
+    @Put("/:id")
+    async update(@Param("id") id: String,@Body() updatedComment: CreatePostCommentDTO): Promise<PostCommentModel>{
+
+        return await this.postCommentService.update(id,updatedComment)
+
+    }
+
+
+    @Delete("/:id")
+    async delete(@Param("id") id: String): Promise<PostCommentModel>{
+
+        return await this.postCommentService.delete(id)
+
+    }
+
+}
