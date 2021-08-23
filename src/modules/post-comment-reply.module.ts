abbef71ef452c4609870f800825bc1aa79568c2a
ntwari <ntwariegide2@gmail.com>
2021-08-23 20:56:10 +0200

    fit post comment reply

--- src/modules/post-comment-reply.module.ts
+++ src/modules/post-comment-reply.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { postCommentReplyProviders } from 'src/providers/post-comment-reply.provider';
+import { PostCommentReplyService } from 'src/services/post-comment-reply.service';
 import { PostCommentReplyController } from '../controllers/post-comment-reply.controller';
+import { DatabaseModule } from './database.module';
 
 @Module({
-  controllers: [PostCommentReplyController]
+  imports: [DatabaseModule],
+    controllers: [PostCommentReplyController],
+    providers: [
+        PostCommentReplyService,
+        ...postCommentReplyProviders
+    ],
 })
 export class PostCommentReplyModule {}
