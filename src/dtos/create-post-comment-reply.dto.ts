93d0d103435789ce2c6c16e3bae8d5d6162ce990
ntwari <ntwariegide2@gmail.com>
2021-08-23 20:02:12 +0200

    add new dto

---
+++ src/dtos/create-post-comment-reply.dto.ts
@@ -0,0 +1,14 @@
+export class CreatePostCommentReplyDTO{
+    
+    commentedBy: String
+
+    commentId: String
+
+    post: String
+  
+    commentedAt: Date
+
+    comment: String
+
+    numberOfLikes: Number
+}
