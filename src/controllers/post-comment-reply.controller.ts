374f36847339ad3a439b0fec5d694e5ee6593e63
ntwari <ntwariegide2@gmail.com>
2021-08-23 23:13:42 +0200

    changes are being processed

--- src/controllers/post-comment-reply.controller.ts
+++ src/controllers/post-comment-reply.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { Body, Delete, Post, Put } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { CreatePostCommentReplyDTO } from 'src/dtos/create-post-comment-reply.dto';
+import PostCommentReplyModel from 'src/interface/post-comment-reply.interface';
+import { PostCommentReplyService } from 'src/services/post-comment-reply.service';
 
-@Controller('post-comment-reply')
-export class PostCommentReplyController {}
+@Controller('/api/v1/post-comment-reply')
+export class PostCommentReplyController {
+
+    constructor(private readonly postCommentReplyService: PostCommentReplyService){}
+
+    @Get()
+    async getALL(): Promise<PostCommentReplyModel[]>{
+        return this.postCommentReplyService.allReplies();
+    }
+
+    @Get('/{id}')
+    async get(@Param("id") id: String): Promise<PostCommentReplyModel>{
+        return this.postCommentReplyService.replybyid(id)
+    }
+
+    @Post()
+
+    async post(@Body() newreply: CreatePostCommentReplyDTO): Promise<PostCommentReplyModel> {
+
+        return this.postCommentReplyService.saveReply(newreply);
+
+    }
+
+    @Put('/{id}')
+    async update(@Body() newreply: CreatePostCommentReplyDTO,@Param('id') id: String ){
+
+        return this.postCommentReplyService.updateReply(newreply,id)
+
+    }
+
+    @Delete('/{id}')
+    async delete(@Param('id') id: String ){
+
+        return this.postCommentReplyService.delete(id)
+
+    }
+}
